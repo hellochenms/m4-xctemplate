42b2d5587f43c92a9825b3be60575ac056b7ae55
chenmeisong <chenmeisong@baidu.com>
2019-07-04 21:53:17 +0800

    m4-swift

---
+++ m4-swift.xctemplate/UITableViewCellSwift/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    // init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubViews()
+        makeLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    private func addSubViews() {
+        
+    }
+    
+    private func makeLayout() {
+        
+    }
+}
---
+++ m4-swift.xctemplate/UIViewSwift/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_cocoaTouchSubclass___ {
+
+    // init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubViews()
+        makeLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    private func addSubViews() {
+        
+    }
+    
+    private func makeLayout() {
+        
+    }
+}
